cac62d21fc9d365c6521056ca70985de3ac1ae74
zacki001 <53890644+zacki001@users.noreply.github.com>
2022-09-12 15:46:10 +0700

    Create tugas_2.php

---
+++ tugas_2.php
@@ -0,0 +1,18 @@
+  <?php
+    
+    // Diberikan array seperti dibawah ini
+    $arr= array(1,2,3,4,5);
+    // Carilah nilai array dan indexnya dengan kondisi
+    // nilai target lebih kecil dari 4 dan lebih besar dari 2;
+    $target = NULL;
+    $targetIndex = NULL;
+    // Lengkapi kode dibawah ini ganti ... dengan kode yang sesuai
+    foreach($arr as $key => $value) {
+        if($value<4 && $value>2) {
+            $target = $value;
+            $targetIndex = $key;
+        }
+    }
+    echo "ketemu nih, aku ada di index $targetIndex dengan nilai $target";
+    
+    ?>
